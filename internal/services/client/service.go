1ade2390f7f14a28ede432a71d4e59cde9db7f67
amperov <alex.amperov@yandex.ru>
2023-03-07 00:33:55 +0700

    Revert "Revert "update""
    This reverts commit b4d12357dd2def8affe8c09b7e11fdcf30869b1d.

--- internal/services/client/service.go
+++ internal/services/client/service.go
@@ -99,7 +99,7 @@ func (c *ClientService) Get(ctx context.Context, UniqueCode string, Username str
 			prod["date_check"] = MapForHistory["date_check"]
 			prod["unique_code"] = MapForHistory["unique_code"]
 			prod["unique_inv"] = MapForHistory["unique_inv"]
-			content += " \n " + prod["content_key"].(string)
+			content += prod["content_key"].(string) + " \n "
 		}
 
 		pkg.SendMessage(Message, MapForHistory["unique_inv"].(int), Token)
