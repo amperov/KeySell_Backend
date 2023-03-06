edec8a621fe64de5a7741cd12be0932739c9d883
amperov <alex.amperov@yandex.ru>
2023-03-07 00:33:56 +0700

    Revert "Revert "update""
    This reverts commit 6362ca60f7ccd76f1597de79ac8cb24a40552100.

--- main.go
+++ main.go
@@ -1,76 +1,99 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"sort"
-	"strconv"
-	"strings"
-)
-
-func main() {
-	sum := 0
-	partValues := "1000, 1500, 2000, 2500, 3000, 5000"
-	target := 10000
-
-	///
-	split := strings.Split(partValues, ", ")
-	///
-	var NewArr []int
-
-	for _, s := range split {
-		log.Println("We in cycle")
-		Num, err := strconv.Atoi(s)
-		NewArr = append(NewArr, Num)
-		if err != nil {
-			log.Println(err)
-		}
-	}
-
-	sort.Ints(NewArr)
-	for i, j := 0, len(NewArr)-1; i < j; i, j = i+1, j-1 {
-		NewArr[i], NewArr[j] = NewArr[j], NewArr[i]
-	}
-
-	fmt.Println(NewArr)
-
-	arr := []int{1500, 1500, 1500, 2500, 1000, 2000, 1500, 1000, 2500, 3000, 5000}
-
-	MapValuesCounters := make(map[int]int)
-
-	for sum <= target {
-
-		for i := 0; i < len(NewArr); i++ {
-
-			if (target-sum)/NewArr[i] <= (SelectCount(arr, NewArr[i])) {
-				MapValuesCounters[NewArr[i]] = (target - sum) / NewArr[i]
-				sum += (NewArr[i]) * target / (NewArr[i])
-			} else if (target-sum)/NewArr[i] > SelectCount(arr, NewArr[i]) {
-				sum += NewArr[i] * (SelectCount(arr, NewArr[i]))
-				MapValuesCounters[NewArr[i]] = SelectCount(arr, NewArr[i])
-			}
-
-			if sum > target {
-				i = 0
-				sum = 0
-			}
-			log.Println(MapValuesCounters)
-			if sum == target {
-				break
-			}
-		}
-
-	}
-}
-
-func SelectCount(arr []int, target int) int {
-	counter := 0
-
-	for _, i2 := range arr {
-		if i2 == target {
-			counter++
-		}
-	}
-	return counter
-
-}
+//
+//import (
+//	"bytes"
+//	"crypto/sha256"
+//	"encoding/hex"
+//	"encoding/json"
+//	"github.com/sirupsen/logrus"
+//	"io"
+//	"net/http"
+//	"strconv"
+//	"time"
+//)
+//
+//type DigiInput struct {
+//	Retval          int     `json:"retval,omitempty"`
+//	Retdesc         string  `json:"retdesc,omitempty"`
+//	Inv             int     `json:"inv,omitempty"`
+//	IdGoods         int     `json:"id_goods,omitempty"`
+//	Amount          float64 `json:"amount,omitempty"`
+//	TypeCurr        string  `json:"type_curr,omitempty"`
+//	Profit          float64 `json:"profit,omitempty"`
+//	AmountUsd       float64 `json:"amount_usd,omitempty"`
+//	DatePay         string  `json:"date_pay,omitempty"`
+//	Email           string  `json:"email,omitempty"`
+//	AgentId         int     `json:"agent_id,omitempty"`
+//	AgentPercent    float64 `json:"agent_percent,omitempty"`
+//	UnitGoods       int     `json:"unit_goods,omitempty"`
+//	CntGoods        float64 `json:"cnt_goods,omitempty"`
+//	PromoCode       string  `json:"promo_code,omitempty"`
+//	BonusCode       string  `json:"bonus_code,omitempty"`
+//	CartUid         string  `json:"cart_uid,omitempty"`
+//	UniqueCodeState struct {
+//		State         int    `json:"state,omitempty"`
+//		DateCheck     string `json:"date_check,omitempty"`
+//		DateDelivery  string `json:"date_delivery,omitempty"`
+//		DateConfirmed string `json:"date_confirmed,omitempty"`
+//		DateRefuted   string `json:"date_refuted,omitempty"`
+//	} `json:"unique_code_state,omitempty"`
+//	Options []struct {
+//		Id    int    `json:"id,omitempty"`
+//		Name  string `json:"name,omitempty"`
+//		Value string `json:"value,omitempty"`
+//	} `json:"options,omitempty"`
+//}
+//type Body struct {
+//	SellerID  int    `json:"seller_id,omitempty"`
+//	Timestamp int64  `json:"timestamp,omitempty"`
+//	Sign      string `json:"sign,omitempty"`
+//}
+//
+//type Resp struct {
+//	Retval   int    `json:"retval,omitempty"`
+//	Token    string `json:"token,omitempty"`
+//	SellerID int    `json:"seller_id,omitempty"`
+//	Valid    string `json:"valid_thru,omitempty"`
+//}
+//
+//func main() {
+//	var body Body
+//
+//	var respStr Resp
+//	//Searching SellerID by Username or integrate Seller Storage
+//
+//	body.SellerID = 841146
+//	body.Timestamp = time.Now().Unix()
+//
+//	hash := sha256.New()
+//	hash.Write([]byte("3A095A88BA044066B860DD4F18F84A5C" + strconv.Itoa(int(body.Timestamp))))
+//	body.Sign = hex.EncodeToString(hash.Sum(nil))
+//
+//	BodyMarshalled, err := json.Marshal(body)
+//	if err != nil {
+//		logrus.Debugf("marshalling error: %v", err)
+//		return
+//	}
+//	reader := bytes.NewReader(BodyMarshalled)
+//
+//	resp, err := http.Post("https://api.digiseller.ru/api/apilogin", "application/json", reader)
+//	if err != nil {
+//		logrus.Debugf("http.Post error: %v", err)
+//		return
+//	}
+//
+//	respBody, err := io.ReadAll(resp.Body)
+//	if err != nil {
+//		logrus.Debugf("ReadAll error: %v", err)
+//		return
+//	}
+//
+//	err = json.Unmarshal(respBody, &respStr)
+//	if err != nil {
+//		logrus.Debugf("unmarshal error: %v", err)
+//		return
+//	}
+//	logrus.Println(respStr)
+//}
+//*/
