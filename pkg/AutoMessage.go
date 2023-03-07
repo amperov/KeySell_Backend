a1e9a0896839dc8bb268f6b73ff8cbc4e7a7f3b3
amperov <alex.amperov@yandex.ru>
2023-03-07 23:27:31 +0700

    update

--- pkg/AutoMessage.go
+++ pkg/AutoMessage.go
@@ -3,17 +3,26 @@ package pkg
 import (
 	"bytes"
 	"fmt"
+	"github.com/sirupsen/logrus"
+	"io"
 	"net/http"
 )
 
 func SendMessage(Message string, InvoiceID int, Token string) error {
 
+	logrus.Printf("InvoiceID: %d \n Token: %s\nMessage: %s", InvoiceID, Token, Message)
 	bodyBytes := []byte(fmt.Sprintf(`{"message": "%s"}`, Message))
 	Body := bytes.NewReader(bodyBytes)
 	Response, err := http.Post(fmt.Sprintf("https://api.digiseller.ru/api/debates/v2/?token=%s&id_i=%d", Token, InvoiceID), "application/json", Body)
 	if err != nil {
 		return err
 	}
+	all, err := io.ReadAll(Response.Body)
+	if err != nil {
+		logrus.Println("Message [ERROR] ", err)
+		return err
+	}
+	logrus.Println(string(all))
 
 	if Response.StatusCode != 200 {
 		return err
